733e03fe03879821ba70b3920a3fd7104a6d369e
KJC-dev11 <kevinjofre1111@gmail.com>
2024-10-24 19:10:27 -0300

    class y datas listas

---
+++ NUTRICIONISTAPROYECTOFINALG15/src/Modelo/MenuDiario.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author MOON
+ */
+public class MenuDiario {
+    private int codMenu;
+    private String dia;
+    private int caloriasDelMenu;
+    private String estado;
+    private int codDieta;
+
+    public MenuDiario() {
+    }
+
+    public MenuDiario(String dia, int caloriasDelMenu, String estado, int codDieta) {
+        this.dia = dia;
+        this.caloriasDelMenu = caloriasDelMenu;
+        this.estado = estado;
+        this.codDieta = codDieta;
+    }
+
+    public MenuDiario(int codMenu, String dia, int caloriasDelMenu, String estado, int codDieta) {
+        this.codMenu = codMenu;
+        this.dia = dia;
+        this.caloriasDelMenu = caloriasDelMenu;
+        this.estado = estado;
+        this.codDieta = codDieta;
+    }
+
+    public int getCodMenu() {
+        return codMenu;
+    }
+
+    public void setCodMenu(int codMenu) {
+        this.codMenu = codMenu;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public int getCaloriasDelMenu() {
+        return caloriasDelMenu;
+    }
+
+    public void setCaloriasDelMenu(int caloriasDelMenu) {
+        this.caloriasDelMenu = caloriasDelMenu;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getCodDieta() {
+        return codDieta;
+    }
+
+    public void setCodDieta(int codDieta) {
+        this.codDieta = codDieta;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuDiario{" + "codMenu=" + codMenu + ", dia=" + dia + ", caloriasDelMenu=" + caloriasDelMenu + ", estado=" + estado + ", codDieta=" + codDieta + '}';
+    }
+    
+    
+    
+}
---
+++ NUTRICIONISTAPROYECTOFINALG15/src/Persistencia/AlimentoData.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Persistencia;
+
+import java.sql.Connection;
+import Modelo.Alimentos;
+import Modelo.Conexion;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author MOON
+ */
+public class AlimentoData {
+    
+    private Connection con = null;
+    
+        public AlimentoData(){
+        con = Conexion.getConexion();
+    }
+    
+        public void guardarAlimento(Alimentos alimento) {
+        String sql = "INSERT INTO alimentos (nombreComida, caloriasPorPorcion, tipoComida, detalle) VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, alimento.getNombreComida());
+            statement.setInt(2, alimento.getCaloriasPorPorcion());
+            statement.setString(3, alimento.getStipoComida());
+            statement.setString(4, alimento.getDetalle());
+            statement.executeUpdate();
+            System.out.println("Alimento guardado con exito.");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+        
+        public void actualizarAlimento(Alimentos alimento) {
+        String sql = "UPDATE alimentos SET nombreComida = ?, caloriasPorPorcion = ?, tipoComida = ?, detalle = ? WHERE codComida = ?";
+        try {
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, alimento.getNombreComida());
+            statement.setInt(2, alimento.getCaloriasPorPorcion());
+            statement.setString(3, alimento.getStipoComida());
+            statement.setString(4, alimento.getDetalle());
+            statement.setInt(5, alimento.getCodComida());
+            statement.executeUpdate();
+            System.out.println("Alimento actualizado con éxito.");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+        
+         public void eliminarAlimento(int codComida) {
+        String sql = "DELETE FROM alimentos WHERE codComida = ?";
+        try {
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setInt(1, codComida);
+            statement.executeUpdate();
+            System.out.println("Alimento eliminado con éxito.");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+         
+         public Alimentos buscarAlimento(int codComida) {
+        String sql = "SELECT * FROM alimentos WHERE codComida = ?";
+        Alimentos alimento = null;
+        try {
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setInt(1, codComida);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                alimento = new Alimentos();
+                alimento.setCodComida(resultSet.getInt("codComida"));
+                alimento.setNombreComida(resultSet.getString("nombreComida"));
+                alimento.setCaloriasPorPorcion(resultSet.getInt("caloriasPorPorcion"));
+                alimento.setStipoComida(resultSet.getString("tipoComida"));
+                alimento.setDetalle(resultSet.getString("detalle"));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return alimento;
+    }
+         
+         public List<Alimentos> listarAlimentos() {
+        List<Alimentos> alimentos = new ArrayList<>();
+        String sql = "SELECT * FROM alimentos";
+        try {
+            PreparedStatement statement = con.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Alimentos alimento = new Alimentos();
+                alimento.setCodComida(resultSet.getInt("codComida"));
+                alimento.setNombreComida(resultSet.getString("nombreComida"));
+                alimento.setCaloriasPorPorcion(resultSet.getInt("caloriasPorPorcion"));
+                alimento.setStipoComida(resultSet.getString("tipoComida"));
+                alimento.setDetalle(resultSet.getString("detalle"));
+                alimentos.add(alimento);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return alimentos;
+    }
+}
